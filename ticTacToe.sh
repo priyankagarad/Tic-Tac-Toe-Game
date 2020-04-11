cf7997f847249e6f9257028fe98485d27e6519f6
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-12 02:21:40 +0530

    ADDED:User Play Game

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -37,10 +37,49 @@ function switchPlayer()
 	if [[ $player == "user" ]]
 	then
   	  echo "$user"
-   else
-     echo "$computer"
    fi
 }
+#  For user play
+function userPlay()
+{
+	if [[ $cell_Count -lt $max_Cell ]]
+   then
+		read -p "Enter Number Between 1 to 9:" position
+      if [[ ${board[$position-1]} -eq $position ]]
+      then
+         board[$position-1]=$user
+         ((cell_Count++))
+         displayBoard
+         rowColumnDiagonalWin
+      else
+         echo "Invalid Cell"
+         userPlay
+      fi
+
+	else
+      echo "Game tie !!"
+      exit
+	fi
+}
+#function to check winning condition for Row Column and Diagonal
+function rowColumnDiagonalWin()
+{
+   diagonal=0
+   column=0
+   for((row=0;row<7;row=row+3))
+   do
+     if [[ ${board[$row]} == ${board[$row+1]} && ${board[$row+1]} == ${board[$row+2]} ]] ||
+        [[ ${board[$column]} == ${board[$column+3]} && ${board[$column+3]} ==  ${board[$column+6]} ]] ||
+        [[ ${board[$diagonal]} == ${board[$diagonal+4]} && ${board[$diagonal+4]} == ${board[$diagonal+8]} ]] ||
+        [[ ${board[$diagonal+2]} == ${board[$diagonal+4]} && ${board[$diagonal+4]} == ${board[$diagonal+6]} ]]
+     then
+        echo "$player Win"
+        exit
+     fi
+     column=$((column+1))
+   done
+}
+
 #to show Board Condition
 function checkCondition()
 {
